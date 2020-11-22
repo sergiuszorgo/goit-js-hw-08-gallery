17a93082b37a90228a277f0da38a36532d4f971a
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-11-22 19:08:05 +0200

    Почти доделал

---
+++ script.js
@@ -0,0 +1,57 @@
+import gallery from './gallery-items.js';
+const jsGallery = document.querySelector('.js-gallery');
+const jsLightbox = document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const buttonClose = document.querySelector('.lightbox__button');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
+
+// === СОЗДАЁМ ГАЛЕРЕЮ ===
+let countImg = 0;
+const newGallery = gallery.map(({description, preview, original})=>{
+  return `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image"
+   src=${preview} data-source=${original} data-count=${countImg++} alt=${description}/></a></li>`;
+}).join('');
+jsGallery.innerHTML = newGallery;
+
+// === НАВЕШИВАЕМ СЛУШАТЕЛИ ===
+jsGallery.addEventListener("click", openModal);
+buttonClose.addEventListener("click", closeModal)
+lightboxOverlay.addEventListener("click", closeModal)
+document.addEventListener('keydown', sliDer);
+
+// === РАСПИСЫВАЕМ ФУНКЦИИ ===
+function openModal(event) {
+    event.preventDefault();
+    jsLightbox.classList.add('is-open');
+    lightboxImage.src = event.target.dataset.source;
+    window.addEventListener('keydown', pressESC);
+};
+function closeModal(event) {
+    event.preventDefault();
+    jsLightbox.classList.remove('is-open');
+    lightboxImage.src = ""
+};
+function pressESC(event) {
+    if (event.code === 'Escape') {
+        closeModal(event);
+    }
+};
+function sliDer(event) {
+    const indexImg = gallery.map(elem => elem.original).indexOf(lightboxImage.src)
+    if (event.key === 'ArrowRight' && jsLightbox.classList.contains('is-open')) {
+        if (indexImg < gallery.length - 1) {
+            return lightboxImage.src = gallery[indexImg + 1].original;
+        }
+        else {
+            return lightboxImage.src = gallery[0].original;
+        }
+    }
+    if (event.key === 'ArrowLeft' && jsLightbox.classList.contains('is-open')) {
+        if (indexImg > 0) {
+        return lightboxImage.src = gallery[indexImg - 1].original;
+        }
+        else {
+            return lightboxImage.src = gallery[gallery.length - 1].original;
+        }
+    }
+    };
